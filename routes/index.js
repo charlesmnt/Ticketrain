ea584a61734d20acf1b2c23720d1b51dd9a40b8a
fredrik Andersson <fandersson1121@gmail.com>
2022-01-27 12:26:33 +0100

    resolve conflicts

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const UserModer = require("../models/users"); 
+const UserModel = require("../models/users"); 
 const OrderModel = require("../models/orders"); 
 const JourneyModel = require("../models/journeys");
 var router = express.Router();
@@ -10,15 +10,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-<<<<<<< HEAD
 router.post('/sign-in', async function(req, res, next) {
 
 
   res.render('index', { title: 'Express' });
-=======
 router.get("/home", (req, res) => {
   res.render("home");
->>>>>>> 499d19bfe92a4ad9247e174a005ae34927bcf0c0
 });
 
 module.exports = router;
