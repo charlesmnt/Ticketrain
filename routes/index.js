ee473a7911aac50f931e77de68c2754e7230ea9f
Charles M <charle.monot@gmail.com>
2022-01-27 15:50:04 +0100

    add Mylasttrips

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.post('/sign-in', async function(req, res, next) {
     var user = await UserModel.findOne({email : req.body.email , password : req.body.password})
-    console.log(user);
-    if (user != null) {
-      res.redirect('/home');
+     if (user != null) {
+      req.session.userId = user._id;
+      console.log(req.session.userId);
+      res.render('home');
+      
     } else {
       res.redirect('/');
     }
@@ -32,6 +34,7 @@ router.post('/sign-in', async function(req, res, next) {
       password: req.body.password,
     })
     var userSaved = await newUser.save();
+    req.session.userId = userSaved._id;
     res.redirect('/home')
   } else {
     res.redirect('/');
@@ -39,17 +42,29 @@ router.post('/sign-in', async function(req, res, next) {
 });
 
 router.get("/home", (req, res) => {
-  res.render("home");
+  
+  if (req.session.userId == null) {
+    res.redirect('/');
+  } else {
+    res.render("home");
+  }
 });
 
-router.get("/home", (req, res) => {
-  res.render("home");
-});  
 
 router.post("/search-journey", (req, res) => {
   console.log(req.body);
   res.render("journeys"); 
 });
 
+router.get("/MyLastTrips", async function (req, res) {
+  
+  if (req.session.userId == null) {
+    res.redirect('/');
+  } else {
+    var MyLastOrder = await UserModel.findById(req.session.userId).populate({path: 'orders', populate:{path: 'journeys', model: OrderModel }}).exec();
+    console.log(MyLastOrder);
+    res.render("home");
+  }
+});
 
 module.exports = router;
