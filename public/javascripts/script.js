44f41952a3c603d7cfcc8c1fe6d54e78bad73005
fredrik Andersson <fandersson1121@gmail.com>
2022-01-28 12:17:45 +0100

    add return option, add delete function, rerender search journeys

---
+++ public/javascripts/script.js
@@ -0,0 +1,25 @@
+$("#return-oneway").click(() => {
+  console.log("Clicked"); 
+  let value = $("#label").text(); 
+  console.log(value); 
+  if(value === "Return") {
+    $("#label").text("One-Way");
+  } else {
+    $("#label").text("Return");
+  }
+  $("#return").toggle();
+});
+
+
+//Form Basket 
+$('#myForm').submit(function(e){
+  e.preventDefault();
+  $.ajax({
+      url: '/add',
+      type: 'post',
+      data:$('#myForm').serialize(),
+      success:function(){
+          console.log((data));
+      }
+  });
+});
